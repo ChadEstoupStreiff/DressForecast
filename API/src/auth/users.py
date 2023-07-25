432b8bcb21daa6ace4ac8b7b595e9c883c357e5a
ChadOW <chad.estoup@gmail.com>
2023-07-25 15:27:39 +0200

    Refractor api name

---
+++ API/src/auth/users.py
@@ -0,0 +1,49 @@
+from pydantic import BaseModel, Field, EmailStr
+from auth.auth_handler import signJWT
+
+
+class UserSchema(BaseModel):
+    fullname: str = Field(...)
+    email: EmailStr = Field(...)
+    password: str = Field(...)
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "fullname": "Abdulazeez Abdulazeez Adeshina",
+                "email": "abdulazeez@x.com",
+                "password": "weakpassword"
+            }
+        }
+
+
+class UserLoginSchema(BaseModel):
+    email: EmailStr = Field(...)
+    password: str = Field(...)
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "email": "abdulazeez@x.com",
+                "password": "weakpassword"
+            }
+        }
+
+
+users = []
+
+
+def get_token(mail: EmailStr):
+    return signJWT(mail)
+
+
+def register_user(user: UserSchema):
+    users.append(user)
+    return get_token(user.email)
+
+
+def check_user(data: UserLoginSchema):
+    for user in users:
+        if user.email == data.email and user.password == data.password:
+            return True
+        return False

51b999e232a5fbf1a53f946d51b1d13efd1211c5
Rexmicool <matthieu.barraud@gmail.com>
2023-07-26 14:43:10 +0200

    lzea

--- API/src/visual_crossing_api.py
+++ API/src/visual_crossing_api.py
@@ -1,2 +1,25 @@
+import urllib.request
+import sys
+import json
+
 def get_weather_week():
-    return []
+    try: 
+        ResultBytes = urllib.request.urlopen("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/paris?unitGroup=metric&key=YOUR_API_KEY&contentType=json")
+        
+        # Parse the results as JSON
+        jsonData = json.load(ResultBytes)
+            
+    except urllib.error.HTTPError  as e:
+        ErrorInfo= e.read().decode() 
+        print('Error code: ', e.code, ErrorInfo)
+        sys.exit()
+    except  urllib.error.URLError as e:
+        ErrorInfo= e.read().decode() 
+        print('Error code: ', e.code,ErrorInfo)
+        sys.exit()
+
+    return jsonData
+
+
+
+                
